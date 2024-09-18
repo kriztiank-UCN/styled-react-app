b0007afd7be971a869e4c5aadad1b6bbf66591f2
kk <kriztiank@gmail.com>
2024-09-18 16:35:48 +0200

    typography function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from "./components/Header";
 import Box from "./components/Box";
 import { ThemeProvider } from "styled-components";
 import theme from "./style/theme";
@@ -5,6 +6,9 @@ import theme from "./style/theme";
 const App = () => (
   <ThemeProvider theme={theme}>
     {/* application elements */}
+    <Header fontFamily="Helvetica" fontSize={3}>
+      Hello!
+    </Header>
     <Box ml={64} color="white" bg="navy">
       Blue Box
     </Box>
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { typography } from 'styled-system';
+
+const Header = styled.header`
+  ${typography}
+`;
+
+export default Header;
